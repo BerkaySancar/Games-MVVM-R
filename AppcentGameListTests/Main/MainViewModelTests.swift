bb51af8ae3b05302769d16e98defd55a07dc1690
Berkay Sancar <sancarberkay@gmail.com>
2023-03-21 16:33:58 +0300

    Adds main mocks

---
+++ AppcentGameListTests/Main/MainViewModelTests.swift
@@ -0,0 +1,67 @@
+//
+//  MainViewModelTests.swift
+//  AppcentGameListTests
+//
+//  Created by Berkay Sancar on 16.03.2023.
+//
+
+import Foundation
+import XCTest
+@testable import AppcentGameList
+
+final class MainViewModelTests: XCTestCase {
+    
+    private var viewModel: MainViewModel!
+    private var view: MockMainView!
+    private var router: MockMainRouter!
+    private var api: MockGameAPI!
+    
+    override func setUp() {
+        super.setUp()
+        
+        view = .init()
+        router = .init()
+        api = .init()
+        viewModel = .init(delegate: view, router: router, gameAPI: api)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        view = nil
+        router = nil
+        api = nil
+        viewModel = nil
+    }
+    
+    func test_viewDidLoad_invokesRequiredMethods() {
+        XCTAssertEqual(view.invokedPrepareCollectionView, 0)
+        XCTAssertEqual(view.invokedPrepareAIV, 0)
+        XCTAssertEqual(view.invokedSetViewBackgroundColor, 0)
+        XCTAssertEqual(view.invokedPrepareSearchController, 0)
+        XCTAssertEqual(api.invokedGetGameList, 0)
+        
+        viewModel.viewDidLoad()
+        
+        XCTAssertEqual(view.invokedPrepareCollectionView, 1)
+        XCTAssertEqual(view.invokedPrepareAIV, 1)
+        XCTAssertEqual(view.invokedSetViewBackgroundColor, 1)
+        XCTAssertEqual(view.invokedPrepareSearchController, 1)
+        XCTAssertEqual(api.invokedGetGameList, 1)
+    }
+    
+    func test_numberOfSection_Equals_2() {
+        XCTAssertEqual(viewModel.numberOfSection(), 2)
+    }
+    
+//    func test_didSelectItemAt_WithAllIndex_RouteToDetail() {
+//        XCTAssertEqual(router.invokedToDetailCount, 0)
+//        XCTAssertNil(router.invokedID)
+//        viewModel.games.append(Game(id: 5, name: "game1", metacritic: nil, backgroundImage: nil, released: nil))
+//
+//        viewModel.didSelectItemAt(index: 0)
+//
+//        XCTAssertEqual(router.invokedToDetailCount, 1)
+//        XCTAssertEqual(router.invokedID, 5)
+//    }
+}
---
+++ AppcentGameListTests/Main/MockMainView.swift
@@ -0,0 +1,51 @@
+//
+//  MockMainView.swift
+//  AppcentGameListTests
+//
+//  Created by Berkay Sancar on 16.03.2023.
+//
+
+import Foundation
+@testable import AppcentGameList
+
+final class MockMainView: MainViewModelOutputs {
+    
+    var invokedSetViewBackgroundColor = 0
+    func setViewBackgroundColor() {
+        invokedSetViewBackgroundColor += 1
+    }
+    
+    var invokedPrepareCollectionView = 0
+    func prepareCollectionView() {
+        invokedPrepareCollectionView += 1
+    }
+    
+    var invokedPrepareAIV = 0
+    func prepareActivtyIndicatorView() {
+        invokedPrepareAIV += 1
+    }
+    
+    var invokedPrepareSearchController = 0
+    func prepareSearchController() {
+        invokedPrepareSearchController += 1
+    }
+    
+    var invokedBeginRefreshing = 0
+    func beginRefreshing() {
+        invokedBeginRefreshing += 1
+    }
+    
+    var invokedEndRefreshing = 0
+    func endRefreshing() {
+        invokedEndRefreshing += 1
+    }
+    
+    var invokedDataRefreshed = 0
+    func dataRefreshed() {
+        invokedDataRefreshed += 1
+    }
+    
+    func onError(message: String) {
+        
+    }
+}
